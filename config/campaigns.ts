b2ed59462d57cbd556bf767ce2f1e56130a613b3
ReBestDigital <alessio.dad.danceshoes@gmail.com>
2026-03-14 18:20:46 +0100

    homesellerclpage

--- config/campaigns.ts
+++ config/campaigns.ts
@@ -38,6 +38,38 @@ export const CAMPAIGNS = {
   footerNote: "Please check your Spam or Promotions folder if you don't see the mail with the download link.",
 }
 },
+"stop": {
+    id: "stop",
+    title: "Own your Business, stop paying for someone else growth...",
+    subtitle: "Our book explain how to do it. Download the Infographics to escape the portals slavery.",
+    text1 : "",
+    text2bold : "",
+    text3border : "",
+    leadMagnet: "the Infographics",
+    buttonLabel: "Get the infographics in your inbox!",
+    image: "/CopertinaDefinitivaPerKindle.jpg", // Percorso della copertina
+    //style: BRAND_STYLES["modern-blue"], // Collega lo stile
+    style: BRAND_STYLES["dark-magenta"], // Collega lo stile
+    legal: PRIVACY_DOCS["standard-v1"],  // Collega la privacy
+    brevoListId: 3,
+    brevotemplateId : 8 ,
+    thankYou: {
+  icon: "🚀",
+  title: "Confirmation Received!",
+  description: "Thank you for confirming your email.",
+  highlight: "Your infographic",
+  subDescription: "is on its way to your inbox.",
+ ctaLabel: "Here you can find the roadmap",
+  ctaLink: SOCIALS["rebest-digital"].site,
+  secondaryLabel: "ReBest Instagram",
+  secondaryLink: SOCIALS["rebest-digital"].instagram,
+  ctaLabel3: "You tube secret playlist",
+  ctaLink3: "https://youtube.com/yourchannel",
+  ctaLink4: "https://wa.me/tuonumero", // Aggiungi questo
+  ctaLabel4: "Facebook Vault",   // E questo
+  footerNote: "Please check your Spam or Promotions folder if you don't see the mail with the download link.",
+}
+},
 
 "bird": {
     //used in earlybird page
